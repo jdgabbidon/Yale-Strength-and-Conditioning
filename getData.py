c4efaece33b05f8a5af7352542d1642415f5f076
seunomonije <oseun22@gmail.com>
2019-11-19 23:47:43 -0500

    work 11:47

--- getData.py
+++ getData.py
@@ -10,11 +10,13 @@ lst = []
 with open('data.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
+        if (row[3] == "Day " or row[3] == "Day"):
+            print("\n\nWORKING\n\n")
+            continue
         lst.append((row[1], row[2], row[3]))
             ## print(row[0])
             ##print(row[0],row[1],row[2],)
 
-print(lst)
 lst = list(set(lst))
 
 with open('athlete.csv', 'w', newline='') as csvfile:
@@ -22,3 +24,4 @@ with open('athlete.csv', 'w', newline='') as csvfile:
     ##quotechar='|', quoting=csv.QUOTE_MINIMAL
     for row in lst:
         writer.writerow([row[0], row[1], row[2]])
+        
