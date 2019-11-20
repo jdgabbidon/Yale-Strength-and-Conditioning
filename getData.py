44366ae8ab9699c49026d6df6f2c7cb54c8b599e
= <=>
2019-11-20 01:00:44 -0500

    Added code to produce all required .csv files

--- getData.py
+++ getData.py
@@ -5,7 +5,10 @@ import csv
 filename = "data.csv"
 
 
-lst = []
+athlete = []
+workout = []
+exercise_set = []
+athlete_max = []
 
 with open('data.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
@@ -13,15 +16,36 @@ with open('data.csv') as csvfile:
         if (row[3] == "Day " or row[3] == "Day"):
             print("\n\nWORKING\n\n")
             continue
-        lst.append((row[1], row[2], row[3]))
-            ## print(row[0])
+        athlete.append((row[1], row[2], row[3]))
+        workout.append((row[4], row[6]))
+        exercise_set.append((row[4], row[3], row[36], row[35], row[39], row[8]))
+        athlete_max.append((row[4], row[3], row[34]))
+
             ##print(row[0],row[1],row[2],)
 
-lst = list(set(lst))
+athlete = list(set(athlete))
+workout = list(set(workout))
+exercise_set = list(set(exercise_set))
+athlete_max = list(set(athlete_max))
 
 with open('athlete.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     ##quotechar='|', quoting=csv.QUOTE_MINIMAL
-    for row in lst:
+    for row in athlete:
+        writer.writerow([row[0], row[1], row[2]])
+with open('workout.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    ##quotechar='|', quoting=csv.QUOTE_MINIMAL
+    for row in workout:
+        writer.writerow([row[0], row[1]])
+with open('exercise_set.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    ##quotechar='|', quoting=csv.QUOTE_MINIMAL
+    for row in exercise_set:
+        writer.writerow([row[0], row[1], row[2], row[3], row[4], row[5]])
+with open('athlete_max.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    ##quotechar='|', quoting=csv.QUOTE_MINIMAL
+    for row in athlete_max:
         writer.writerow([row[0], row[1], row[2]])
         
